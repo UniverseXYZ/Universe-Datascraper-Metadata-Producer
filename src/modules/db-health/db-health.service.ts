9bdb6801c4585729015f1dbd5ef79d6f2cc42c1c
Kun Wang <kun.w@hotmail.com>
2022-02-22 12:35:17 +1300

    add back substring for message id (#5)

--- src/modules/db-health/db-health.service.ts
+++ src/modules/db-health/db-health.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
 import { HealthIndicator, HealthIndicatorResult } from '@nestjs/terminus';
 import { InjectConnection } from '@nestjs/mongoose';
 import { Connection } from 'mongoose';
 
 @Injectable()
 export class DbHealthService extends HealthIndicator {
+  private readonly logger = new Logger(DbHealthService.name);
+
   constructor(@InjectConnection() private readonly connection: Connection) {
     super();
   }
@@ -16,9 +18,11 @@ export class DbHealthService extends HealthIndicator {
       if (ok) {
         return this.getStatus(key, true, { message: 'DB is healthy' });
       }
-
+      this.logger.error('DB is not healthy');
       return this.getStatus(key, false, { message: 'DB is not healthy' });
     } catch (error) {
+      this.logger.error('DB is not healthy');
+      this.logger.error(error.message);
       return this.getStatus(key, false, { message: error.message });
     }
   }
