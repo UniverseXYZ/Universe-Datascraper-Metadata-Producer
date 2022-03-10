585925e8f2c6c5a9bbd77d7eaf8a0e47143a874c
kunone <kun.w@hotmail.com>
2022-03-10 19:35:38 +1300

    cleanup project eth-health

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -1,7 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
-import { EthHealthIndicator } from '../eth-health/eth-health.service';
 import { DbHealthService } from '../db-health/db-health.service';
 
 @Controller('health')
