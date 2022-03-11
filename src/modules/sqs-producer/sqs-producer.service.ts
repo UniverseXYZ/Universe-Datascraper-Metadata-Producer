0c97eda081d0087e259836e423ba4846e7c01eb2
Ryan Shtirmer <rshtirmer@gmail.com>
2022-03-11 12:56:31 -0500

    Revert "process failed metadata every minute (#9)"
    This reverts commit 4f92441fd79024683c0e4276fb50b93c71072183.

--- src/modules/sqs-producer/sqs-producer.service.ts
+++ src/modules/sqs-producer/sqs-producer.service.ts
@@ -140,50 +140,45 @@ export class SqsProducerService implements OnModuleInit, SqsProducerHandler {
    * If the token's metadata failed to fetch, it will send a message to queue to refresh the metadata.
    * TODO: to update to use batch
    */
-  @Cron(CronExpression.EVERY_MINUTE)
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   public async checkToRefetchMetadata() {
     // Check if there is any failed collection
-    const failed = await this.nftTokenService.findFailed();
-    if (!failed || failed.length === 0) {
+    const failedOne = await this.nftTokenService.findFailedOne();
+    if (!failedOne) {
       return;
     }
     this.logger.log(
-      `[CRON Token - ReSync] Find ${failed.length} failed tokens`,
+      `[CRON Token - ReSync] Recevied metadata fetched failed token: ${failedOne.contractAddress} - ${failedOne.tokenId}`,
     );
-    for(const failedOne of failed) {
-      this.logger.log(
-        `[CRON Token - ReSync] Recevied metadata fetched failed token: ${failedOne.contractAddress} - ${failedOne.tokenId}`,
-      );
 
-      // Prepare queue messages and sent as batch
-      const id = `${failedOne.contractAddress}-${failedOne.tokenId.substring(
-        0,
-        30,
-      )}`;
-      const message: Message<QueueMessageBody> = {
-        id,
-        body: {
-          contractAddress: failedOne.contractAddress,
-          contractType: failedOne.tokenType,
-          tokenId: failedOne.tokenId,
-        },
-        groupId: id,
-        deduplicationId: id,
-      };
-      await this.sendMessage(message);
-      this.logger.log(
-        `[CRON Token - ReSync] Successfully sent messages for token ${failedOne.contractAddress} - ${failedOne.tokenId}`,
-      );
+    // Prepare queue messages and sent as batch
+    const id = `${failedOne.contractAddress}-${failedOne.tokenId.substring(
+      0,
+      30,
+    )}`;
+    const message: Message<QueueMessageBody> = {
+      id,
+      body: {
+        contractAddress: failedOne.contractAddress,
+        contractType: failedOne.tokenType,
+        tokenId: failedOne.tokenId,
+      },
+      groupId: id,
+      deduplicationId: id,
+    };
+    await this.sendMessage(message);
+    this.logger.log(
+      `[CRON Token - ReSync] Successfully sent messages for token ${failedOne.contractAddress} - ${failedOne.tokenId}`,
+    );
 
-      // Mark this token
-      await this.nftTokenService.markAsProcessed(
-        failedOne.contractAddress,
-        failedOne.tokenId,
-      );
-      this.logger.log(
-        `[CRON Token - ReSync] Successfully processed token ${failedOne.contractAddress} - ${failedOne.tokenId}`,
-      );
-    }
+    // Mark this token
+    await this.nftTokenService.markAsProcessed(
+      failedOne.contractAddress,
+      failedOne.tokenId,
+    );
+    this.logger.log(
+      `[CRON Token - ReSync] Successfully processed token ${failedOne.contractAddress} - ${failedOne.tokenId}`,
+    );
   }
 
   async sendMessage<T = any>(payload: Message<T> | Message<T>[]) {
