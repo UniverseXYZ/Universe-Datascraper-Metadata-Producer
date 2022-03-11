d4873be2f78108d7a270ba5c5009f6aba1112dc7
Ryan Shtirmer <rshtirmer@gmail.com>
2022-03-11 12:56:38 -0500

    Revert "process failed metadata every minute (#9)" (#10)
    This reverts commit 4f92441fd79024683c0e4276fb50b93c71072183.

--- src/modules/nft-tokens/nft-tokens.service.ts
+++ src/modules/nft-tokens/nft-tokens.service.ts
@@ -45,17 +45,6 @@ export class NFTTokensService {
     });
   }
 
-  async findFailed() {
-    return await this.nftTokensModel.find({
-      sentAt: { $ne: null },
-      metadata: null,
-    },
-    {},
-    {
-      limit: 100
-    });
-  }
-
   public async markAsProcessed(contractAddress: string, tokenId: string) {
     await this.nftTokensModel.updateOne(
       { contractAddress, tokenId },
