72cada829b7e0ebbec3ec9cdce5c466bb3fc0e80
vikinatora <vikinatora@gmail.com>
2022-06-24 09:51:54 +0300

    enable eth health check

--- src/modules/health/health.controller.ts
+++ src/modules/health/health.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { HealthCheckService, HealthCheck } from '@nestjs/terminus';
 import { DbHealthService } from '../db-health/db-health.service';
+import { EthHealthIndicator } from '../eth-health/eth-health.service';
 
 @Controller('health')
 @ApiTags('Health')
@@ -9,12 +10,15 @@ export class HealthController {
   constructor(
     private health: HealthCheckService,
     private db: DbHealthService,
+    private eth: EthHealthIndicator,
   ) {}
 
   @Get()
   @HealthCheck()
   async health_check() {
-    // TODO: check health
-    return this.health.check([() => this.db.pingCheck('mongodb')]);
+    return this.health.check([
+      () => this.db.pingCheck('mongodb'),
+      () => this.eth.pingCheck('ethereum'),
+    ]);
   }
 }
